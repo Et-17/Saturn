6b99e75bf3e718f53c49f60de5780c92282070f0
Et-17 <leland.kilborn@hotmail.com>
2024-11-24 23:02:45 -0500

    Build new ledger data structure

---
+++ src/account_management/ledger_state.ts
@@ -0,0 +1,72 @@
+// We're storing ledgers as three hashmaps that use uuids as keys: transactions,
+// accounts, and counterparties.
+
+import { ref, Ref } from "vue";
+
+// This is the return type of crypto.randomUUID()
+export type UUID = `${string}-${string}-${string}-${string}-${string}`;
+export type UUIDMap<ValueType> = Map<UUID, ValueType>;
+
+export interface Transaction {
+    account_id: UUID;
+    counterparty_id: UUID;
+    amount: number;
+    timestamp: Date;
+}
+
+// All accounts start at $0 in order to make their history more traceable
+export interface Account {
+    name: string;
+    description?: string;
+    balance: number;
+    transactions: UUID[];
+    creation_timestamp: Date;
+}
+
+export interface Counterparty {
+    name: string;
+    description?: string;
+    transactions: UUID[];
+}
+
+export const transactions: Ref<UUIDMap<Transaction>> = ref(new Map());
+export const accounts: Ref<UUIDMap<Account>> = ref(new Map());
+export const counterparties: Ref<UUIDMap<Counterparty>> = ref(new Map());
+
+export async function new_transaction(account_id: UUID, counterparty_id: UUID, amount: number, timestamp?: Date): Promise<void> {
+    let new_uuid = crypto.randomUUID();
+
+    if (!accounts.value.has(account_id)) {
+        throw new Error(`Account id ${account_id} does not exist`);
+    }
+    if (!counterparties.value.has(counterparty_id)) {
+        throw new Error(`Counterparty id ${counterparty_id} does not exist`);
+    }
+    accounts.value.get(account_id).transactions.push(new_uuid);
+    counterparties.value.get(counterparty_id).transactions.push(new_uuid);
+
+    transactions.value.set(crypto.randomUUID(), {
+        account_id,
+        counterparty_id,
+        amount,
+        timestamp: timestamp ?? new Date()
+    });
+}
+
+export async function new_account(name: string, description?: string, creation_timestamp?: Date): Promise<void> {
+    accounts.value.set(crypto.randomUUID(), {
+        name,
+        description,
+        balance: 0,
+        transactions: [],
+        creation_timestamp: creation_timestamp ?? new Date
+    });
+}
+
+export async function new_counterparty(name: string, description?: string): Promise<void> {
+    counterparties.value.set(crypto.randomUUID(), {
+        name,
+        description,
+        transactions: []
+    });
+}
