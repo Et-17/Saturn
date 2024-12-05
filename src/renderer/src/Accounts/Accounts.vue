8f0c470987d6d36b7660b131659fe9658c8aba8d
Et-17 <leland.kilborn@hotmail.com>
2024-12-04 21:34:37 -0500

    Sprits of cleanup

--- src/renderer/src/Accounts/Accounts.vue
+++ src/renderer/src/Accounts/Accounts.vue
@@ -1,25 +1,27 @@
 <script setup lang="ts">
-import { UUID } from '../account_management/ledger_state';
-import AccountsTable from './AccountsTable.vue';
-import { ref, Ref } from 'vue';
-import AccountView from './AccountView.vue';
+import { UUID } from '../account_management/ledger_state'
+import AccountsTable from './AccountsTable.vue'
+import { ref, Ref } from 'vue'
+import AccountView from './AccountView.vue'
 
-const currently_viewing: Ref<UUID | null> = ref(null);
+const currently_viewing: Ref<UUID | null> = ref(null)
 </script>
 
 <template>
   <div id="accounts">
     <div id="accounts-top-bar">
-      <span id="accounts-title">Accounts</span>
-      <span v-if="currently_viewing != null" id="back-button" @click="currently_viewing = null">Back</span>
+      <span id="accounts-title"> Accounts </span>
+      <span v-if="currently_viewing != null" id="back-button" @click="currently_viewing = null">
+        Back
+      </span>
     </div>
-    <AccountsTable v-if="currently_viewing == null" @open-account="(uuid: UUID) => currently_viewing = uuid" />
-    <AccountView v-if="currently_viewing != null" :account_uuid="currently_viewing"></AccountView>
+    <AccountsTable v-if="currently_viewing == null" @open-account="(uuid: UUID) => (currently_viewing = uuid)" />
+    <AccountView v-if="currently_viewing != null" :account_uuid="currently_viewing" />
   </div>
 </template>
 
-<style lang=scss>
-@use "../style_atoms.scss";
+<style lang="scss">
+@use '../style_atoms.scss';
 
 #accounts {
   max-width: 40%;
